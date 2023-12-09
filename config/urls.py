2c25b23fab1fc4e6f2ebda848d0a3547330028df
Trm200202 <ruslan231102@gmail.com>
2023-12-09 15:27:25 +0500

    statoc

--- config/urls.py
+++ config/urls.py
@@ -40,6 +40,8 @@ urlpatterns = [
     path("api/v1/", include("app.urls")),
 ] 
 
-urlpatterns += static(settings.MEDIA_URL,
-                       document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
